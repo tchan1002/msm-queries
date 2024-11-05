252c8d4eed6ceb80698f7eb28ffb58ed61d5537e
tchan1002 <tchan0@uchicago.edu>
2024-11-05 21:05:36 +0000

    triumph?

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -8,4 +8,12 @@ class DirectorsController < ApplicationController
     @the_director = @matching_records.at(0)
     render({:template => "director_templates/details"})
   end
+  def youngest
+    @the_youngest = Director.order({:dob => :desc}).first
+    render({:template => "director_templates/youngest"})
+  end
+  def eldest
+    @the_eldest = Director.order({:dob => :asc}).first
+    render({:template => "director_templates/eldest"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/youngest", { :controller => "directors", :action => "youngest" })
+  get("/directors/eldest", { :controller => "directors", :action => "eldest" })
+  get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:the_id", { :controller => "actors", :action => "show" })
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:the_id", { :controller => "movies", :action => "show" })
   get("/directors/:the_id", { :controller => "directors", :action => "show" })
 end
